304103c846899b899c459bfd0e576b9d9a32180f
Muneera Banu <muneerabanualamur@gmail.com>
2026-06-21 18:17:44 +0530

    fix deployment config

--- app.py
+++ app.py
@@ -168,4 +168,6 @@ def ask():
         return f"AI Error: {str(e)}"
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
